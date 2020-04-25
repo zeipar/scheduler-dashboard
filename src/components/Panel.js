461d2c12649c1f1f45edc43038608ffeaa5fd2d4
Junsung Park <zeipar@gmail.com>
2020-04-25 01:46:27 -0700

    updated Panel component

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -5,9 +5,7 @@ class Panel extends Component {
     const { label, value } = this.props;
 
     return (
-      <section
-        className="dashboard__panel"
-      >
+      <section className="dashboard__panel">
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
