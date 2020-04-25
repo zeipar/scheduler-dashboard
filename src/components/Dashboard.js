75c7bb60aee8a2fd34f5d0d76fe91467194a794d
Junsung Park <zeipar@gmail.com>
2020-04-25 01:32:34 -0700

    added class Loading class and imported to Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,10 +2,41 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
+import Loading from "./Loading";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+
 class Dashboard extends Component {
+  state = { loading: false};
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
+    if (this.state.loading) {
+      return <Loading />;
+    }
     return <main className={dashboardClasses} />;
   }
 }
