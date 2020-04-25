a9c85f410b5abcad8021995ecaa8977ba1096088
Junsung Park <zeipar@gmail.com>
2020-04-25 02:18:42 -0700

    added focused state and bound it to selectPanel instance method. Also passed selectPanel method as reference using arrow function

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,16 +30,33 @@ const data = [
 
 
 class Dashboard extends Component {
-  state = { loading: false};
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  selectPanel(id) {
+
+    this.setState(prev => ({
+      focused: (prev.focused) ? null : id
+    }))
+
+
+  };
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
-    
-    const panels = data.map(panel => <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} />)
+
+    const panels = data
+      .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel => <Panel onSelect={e => this.selectPanel(panel.id)} key={panel.id} id={panel.id} label={panel.label} value={panel.value} />);
+
     return (
       <main className={dashboardClasses}>
         {panels}
